bac1cf3e3bce64215695beab38ce632cb1ac6702
Ruben Gonzalo Soria Soria <rusovsky_c@hotmail.com>
2020-05-28 21:07:30 -0400

    Commit único.

---
+++ BaseDeDatosLectura/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using BaseDeDatosLectura.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BaseDeDatosLectura.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult MostrarDatos() {
+            EjemploASPNETEntities1 db = new EjemploASPNETEntities1();
+            ViewBag.productos = db.Producto.ToList();
+            return View();
+        }
+    }
+}
